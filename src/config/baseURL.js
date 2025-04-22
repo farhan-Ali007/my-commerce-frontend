4c3d2db4a6e983598d06e76a0503d215831f5d0c
farhan-Ali007 <farhanali39765500@gmail.com>
2025-04-22 12:51:23 +0500

    all images optimized

--- src/config/baseURL.js
+++ src/config/baseURL.js
@@ -1,2 +1,1 @@
-// export const BASE_URL = 'https://etimad.up.railway.app/api/v1';
-export const BASE_URL = 'http://localhost:3600/api/v1';
+export const BASE_URL = 'https://etimad.up.railway.app/api/v1';
